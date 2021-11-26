30c5ba6bb8c4b0a247d0cb2e2a0d36efb46e34f5
MarcoBattiston1 <94076616+MarcoBattiston1@users.noreply.github.com>
2021-11-26 23:30:07 +0100

    Creating get_song function

--- mypackage/getsong_fun.py
+++ mypackage/getsong_fun.py
@@ -1,1 +1,18 @@
 #getsong_fun
+
+def get_song1(lyr):
+    lyr = lyr.lower()
+    db = pd.DataFrame(pd.read_csv('spotify_songs_final.csv'))
+    all_lyr = list(db["lyrics"])
+    for i in range(len(all_lyr)):
+        all_lyr[i] = all_lyr[i].lower()
+    n = 0
+    
+    for i in range(len(all_lyr)): #iterating through the each lyrics of the dataset
+        for j in range(len(all_lyr[i-len(lyr)])): #accessing 
+            if lyr == all_lyr[i][j:j+len(lyr)]:
+                print(db["track_name"][i])
+                n +=1
+    if n == 0:
+        print("So sorry! '" + lyr + "' seems not to be found in any song inside our database. Try with" +
+              " something else")

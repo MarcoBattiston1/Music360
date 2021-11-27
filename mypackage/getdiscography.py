d131f4f1227910cac653a4af09027f63ace1983d
MikeMaic <mike.cecconello@student.h-farm.com>
2021-11-28 00:04:31 +0100

    PEP8 compliant modifications

--- mypackage/getdiscography.py
+++ mypackage/getdiscography.py
@@ -1,14 +1,15 @@
-#getdiscography
+'''
+"getdiscography" function
+allows to input an artist name,
+and returns all the songs of that artist
+present in our database.
+'''
 
-'''
-The "getdiscography" function takes as input an artist name
-and returns the whole discography of that artist present
-in our database.
-'''
 
 def getdiscography(artist):
-    songs = songs.lower() 
-    db = pd.DataFrame(pd.read_csv('spotify_songs_final.csv'))
+
+    songs = songs.lower()
+    db = pd.DataFrame(pd.read_csv('spotify_songs.csv'))
     tracks = list(db["track_name"])
     for i in range(len(tracks)):
         tracks[i] = tracks[i].lower()
@@ -17,4 +18,5 @@ def getdiscography(artist):
         ind = tracks.index(songs)
         print(db["lyrics"][ind])
     else:
-        print(songs + " seems not to be present in our database.") #WRONG INPUT CASE: recommendations of artists/songs
+        print(songs + " seems not to be present in our database.")
+#  WRONG INPUT CASE: recommendations of artists/songs

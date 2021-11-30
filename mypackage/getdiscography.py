eb469e36f98cde466254872ea7826ce48c3f6986
piekoonda <94468410+piekoonda@users.noreply.github.com>
2021-11-30 22:10:01 +0100

    Creating Getdiscography function inside artist class

--- mypackage/getdiscography.py
+++ mypackage/getdiscography.py
@@ -1,22 +1,27 @@
 '''
-"getdiscography" function
-allows to input an artist name,
-and returns all the songs of that artist
-present in our database.
+The Artist class contains the getdiscography function which,
+given an artist name as input, returns the whole set of songs
+by that artist which are contained in our dataset.
+If the input is not present in our dataset, the function
+returns some suggestions about valid input
 '''
 
+class Artist:
+    def __init__(self, artist_name):
+        self.artist_name = artist_name
 
-def getdiscography(artist):
-
-    songs = songs.lower()
-    db = pd.DataFrame(pd.read_csv('spotify_songs.csv'))
-    tracks = list(db["track_name"])
-    for i in range(len(tracks)):
-        tracks[i] = tracks[i].lower()
-
-    if songs in tracks:
-        ind = tracks.index(songs)
-        print(db["lyrics"][ind])
-    else:
-        print(songs + " seems not to be present in our database.")
-#  WRONG INPUT CASE: recommendations of artists/songs
+    def getdiscography(artist_name):
+        import pandas as pd
+        artist_name = artist_name.lower().strip()
+        db = pd.DataFrame(pd.read_csv('spotify_songs.csv'))
+        discography = []
+        for i in range(len(db.track_artist)):
+            if artist_name == db.track_artist[i].lower():
+                discography.append(db.track_name[i])
+        if len(discography) ==0:
+            return("The artist you're looking for is not present in our database. "+
+                  "Here you are some recommended artist we have:\n" + 
+                  "'Mariah Carey',\n" +
+                  "'Madison Beer',\n" +
+                  "'GASHI'!")
+        return (print(artist_name.title(), "made the following songs:\n", *discography, sep="\n"))

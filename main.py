b4371d224ca1f86230cd14aa57ec77a9d8861079
piekoonda <94468410+piekoonda@users.noreply.github.com>
2021-11-30 22:36:48 +0100

    Initializing main.py and Argparse

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#main.py file
+from mypackage/getdiscography import Artist
+from mypackage/getinfo import Song
+import argparse
+import pandas as pd
+
+
+db = pd.DataFrame(pd.read_csv('mypackage/spotify_songs.csv'))
+parser = argparse.ArgumentParser(description='This program will' +
+                                             ' give you information' +
+                                             ' about the song you input' +
+                                             ' if it is present in our database.')
+

fdfb1f0868b7e516318a4a1b7184d4ea3970d9de
piekoonda <94468410+piekoonda@users.noreply.github.com>
2021-11-30 22:06:30 +0100

    Creating Getinfo function inside song class

--- mypackage/getinfo.py
+++ mypackage/getinfo.py
@@ -1,29 +1,37 @@
 '''
-"getinfo"" function
-allows to return
-a series of information (artist, album,
-release date, genre and subgenre,
-bpm, playback time and playlist)
-given a song title.
+The Song class contains the getinfo function which,
+given a song title as input, returns information about
+the artist, album, release date, playlist in which it
+appeared, genre and subgenre of the playlist,
+bpm of the song and playback time.
 '''
 
+class Song:
+    def __init__(self, song_name):
+        self.song_name = song_name
 
-def getinfo(song):
+    def getinfo(song_name):
+        import pandas as pd
+        song_name = song_name.lower().strip()
+        db = pd.DataFrame(pd.read_csv('spotify_songs.csv'))
+        counter = 0
+        for i in range(len(db.track_name)):
+            if song_name == db.track_name[i].lower():
+                return(print("The song: '", db.track_name[i],"' is made by '",
+                     db.track_artist[i], "'. It was released in the album '",
+                      db.track_album_name[i], "', in the date", 
+                      db.track_album_release_date[i], ", and it appeared in the spotify playlist named",
+                      db.playlist_name[i], ", with other song of genre",
+                     db.playlist_genre[i], ", and subgenre",
+                     db.playlist_subgenre[i],". The song has",
+                     round(db.tempo[i]),"bpm and it lasts",
+                     round(db.duration_ms[i]/1000),"seconds."))
+                counter += 1
+                break
+        if counter ==0:
+            return(print("The song you're looking for is not present in our database. "+
+                  "Here you are some recommended songs we have:\n" + 
+                  "'Party Rock Anthem',\n" +
+                  "'Symphony Of Destruction',\n" +
+                  "'Some Nights'!"))
 
-    lyr = lyr.lower()
-    db = pd.DataFrame(pd.read_csv('spotify_songs_final.csv'))
-    all_lyr = list(db["lyrics"])
-    for i in range(len(all_lyr)):
-        all_lyr[i] = all_lyr[i].lower()
-    n = 0
-    # iterating through the each lyrics of the dataset
-    for i in range(len(all_lyr)):
-        for j in range(len(all_lyr[i-len(lyr)])):  # accessing
-            if lyr == all_lyr[i][j:j+len(lyr)]:
-                print(db["track_name"][i])
-                n += 1
-    if n == 0:
-        print("So sorry! '" + lyr +
-              " seems not to be found in any song inside our database." +
-              " Try with something else")
-        # WRONG INPUT CASE: Recommend different artists/songs

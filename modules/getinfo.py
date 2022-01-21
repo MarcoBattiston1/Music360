658ac8bfab08b5bc9cc4415ec86a596c7e35840b
MikeMaic <mike.cecconello@student.h-farm.com>
2022-01-21 17:08:17 +0100

    Riorganized files and paths

---
+++ modules/getinfo.py
@@ -0,0 +1,53 @@
+"""
+The Song class contains the getinfo function which,
+given a song title as input, returns information about
+the artist, album, release date, playlist in which it
+appeared, genre and subgenre of the playlist,
+bpm of the song and playback time.
+"""
+import pandas as pd
+import os
+
+
+class Song:
+    def __init__(self, song_name, path):
+        self.song_name = song_name
+        self.path = path
+
+    def getinfo(self):
+        song_name = self.song_name.lower().strip()
+        db = pd.read_csv(self.path)
+        titles = list(db["track_name"].str.lower())
+        if song_name in titles:
+            i = titles.index(song_name)
+            return ("\nThe song: '" + db.track_name[i] + "' is made by '" +
+                    db.track_artist[i] + "'.\nIt was released in the album '" +
+                    db.track_album_name[i] + "', in the date " +
+                    db.track_album_release_date[i] + ", and it appeared in the spotify playlist named " +
+                    db.playlist_name[i] + ", with other song of genre " +
+                    db.playlist_genre[i] + ", and subgenre " +
+                    db.playlist_subgenre[i] + ".\nThe song has " +
+                    str(round(db.tempo[i])) + " bpm and it lasts " +
+                    str(round(db.duration_ms[i] / 1000)) + " seconds.\n")
+        else:
+            return (" The song you're looking for is not present in our database. " +
+                    " Here you are some recommended songs we have:\n " +
+                    " 'Party Rock Anthem',\n " +
+                    " 'Symphony Of Destruction',\n " +
+                    " 'Some Nights'! ")
+
+    def karaoke(self):
+        song_name = self.song_name.lower().strip()
+        db = pd.read_csv(self.path)
+        titles = list(db["track_name"].str.lower())
+        if song_name in titles:
+            i = titles.index(song_name)
+            return (" \nType this in your favourite browser: 'spotify:track:" +
+                    str(db.track_id[i]) +"' and sing with our lyrics!\n\n " +
+                    db.track_name[i].upper() + " - " + db.track_artist[i].upper() + "\n\n" +
+                    str(db.lyrics[i]))
+        else:
+            return ("Not found!\nHere you are some recommended songs we have:\n" +
+                    "'Party Rock Anthem',\n" +
+                    "'Symphony Of Destruction',\n" +
+                    "'Some Nights'!")
---
+++ modules/getsong.py
@@ -0,0 +1,48 @@
+'''
+This module contains a function named get_song,
+which takes as input a string given by the user
+and check if that string is present in any lyrics
+present in the dataset, through an iteration.
+The function returns all the matches find,
+but stops at 50 songs in order to keep the output clear.
+'''
+
+class Lyrics:
+    def __init__(self, lyr, path):
+        self.lyr = lyr
+        self.path = path
+
+    def getsong(self):
+        import pandas as pd
+        lyr = str(self.lyr)
+        lyr = self.lyr.lower()
+        wrong_input = ["", " "]
+        if str(lyr) in wrong_input:
+            return "Empty input is not supported by the program. Try with something different!"
+        db = pd.read_csv(self.path)
+        all_lyr = list(db["lyrics"])
+        for i in range(len(all_lyr)):
+            all_lyr[i] = all_lyr[i].lower()
+        n = 0
+        if "mypackage" in self.path:
+            print("This could take a while...\n")
+        res = ""
+        for i in range(len(all_lyr)):  # iterating through each lyrics of the dataset
+            for j in range(len(all_lyr[i - len(lyr)])):  # accessing each set of words
+                if lyr == all_lyr[i][j:j + len(lyr)]:
+                    res += db["track_name"][i].upper() + " - " + db["track_artist"][i].upper() + "\n"
+                    n += 1
+                    if n >= 50:
+                        return (res + "\nExecution stopped at 50th matching result.\n" + 
+                                "There are more than 50 songs with this input.\n")
+                    break
+
+        if n == 0:
+            return ("So sorry! '" + lyr + "' seems not to be found in any song inside our database.\n\n" +
+                    "You could try with other lyrics, try these for example:\n" +
+                    "'Push it out, fake a smile'\n" +
+                    "'I just wanna stay in the sun where I find'")
+
+        return res
+
+            

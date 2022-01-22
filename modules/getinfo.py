3062a9958c03a91f1bd79d193199a5925ecbc328
piekoonda <94468410+piekoonda@users.noreply.github.com>
2022-01-22 12:58:37 +0100

    PEP8

--- modules/getinfo.py
+++ modules/getinfo.py
@@ -7,27 +7,25 @@ bpm of the song and playback time.
 """
 
 
-#importing needed libraries
+# importing needed libraries
 import pandas as pd
 import os
 
 
-
-#creating a class named Song
+# creating a class named Song
 class Song:
 
-    def __init__(self, song_name, path):        #initializing the object's attributes
+    def __init__(self, song_name, path):  # initializing object
         self.song_name = song_name
         self.path = path
 
-
-    def getinfo(self):                                   #defining the function to return song info
+    def getinfo(self):  # defining the function to return song info
         song_name = self.song_name.lower().strip()
-        db = pd.read_csv(self.path)                      #assigning the database to the variable "db"
-        titles = list(db["track_name"].str.lower())      #creating a list with all song titles 
+        db = pd.read_csv(self.path)  # assigning the database to the variable "db"
+        titles = list(db["track_name"].str.lower())  # creating a list with all song titles
 
         if song_name in titles:
-            i = titles.index(song_name)        #assinging the index number to variable i
+            i = titles.index(song_name)  # assinging the index number to variable i
             return ("\nThe song: '" + db.track_name[i] + "' is made by '" +
                     db.track_artist[i] + "'.\nIt was released in the album '" +
                     db.track_album_name[i] + "', in the date " +
@@ -36,7 +34,7 @@ class Song:
                     db.playlist_genre[i] + ", and subgenre " +
                     db.playlist_subgenre[i] + ".\nThe song has " +
                     str(round(db.tempo[i])) + " bpm and it lasts " +
-                    str(round(db.duration_ms[i] / 1000)) + " seconds.\n")        #returning info
+                    str(round(db.duration_ms[i] / 1000)) + " seconds.\n")  # returning info
         else:
             return (" The song you're looking for is not present in our database. " +
                     " Here you are some recommended songs we have:\n " +
@@ -44,20 +42,19 @@ class Song:
                     " 'Symphony Of Destruction',\n " +
                     " 'Some Nights'! ")
 
-
-    def karaoke(self):                                  #defining the function to create a karaoke
-        song_name = self.song_name.lower().strip()       
+    def karaoke(self):  # defining the function to create a karaoke
+        song_name = self.song_name.lower().strip()
         db = pd.read_csv(self.path)
-        titles = list(db["track_name"].str.lower())     #creating a list with all song titles
-        
+        titles = list(db["track_name"].str.lower())  # creating a list with all song titles
+
         if song_name in titles:
-            i = titles.index(song_name)                 #assinging the index number to variable i
+            i = titles.index(song_name)  # assinging the index number to variable i
             return (" \nType this in your favourite browser: 'spotify:track:" +
-                    str(db.track_id[i]) +"' and sing with our lyrics!\n\n " +
+                    str(db.track_id[i]) + "' and sing with our lyrics!\n\n " +
                     db.track_name[i].upper() + " - " + db.track_artist[i].upper() + "\n\n" +
-                    str(db.lyrics[i]))                  #returning song URL and song lyrics
+                    str(db.lyrics[i]))  # returning song URL and song lyrics
         else:
             return ("Not found!\nHere you are some recommended songs we have:\n" +
                     "'Party Rock Anthem',\n" +
                     "'Symphony Of Destruction',\n" +
-                    "'Some Nights'!")                   #returning recommendations, if song not present
+                    "'Some Nights'!")  # returning recommendations, if song not present

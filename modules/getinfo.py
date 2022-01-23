5d560ad2337e1a6031eb63129cb6b59f78276fee
MikeMaic <mike.cecconello@student.h-farm.com>
2022-01-23 21:29:44 +0100

    PEP8 compliant modifications on modules

--- modules/getinfo.py
+++ modules/getinfo.py
@@ -19,42 +19,57 @@ class Song:
         self.song_name = song_name
         self.path = path
 
-    def getinfo(self):  # defining the function to return song info
+    # defining the function to return song info
+    def getinfo(self):
         song_name = self.song_name.lower().strip()
-        db = pd.read_csv(self.path)  # assigning the database to the variable "db"
-        titles = list(db["track_name"].str.lower())  # creating a list with all song titles
+        # assigning the database to the variable "db"
+        db = pd.read_csv(self.path)
+        # creating a list with all song titles
+        titles = list(db["track_name"].str.lower())
 
         if song_name in titles:
-            i = titles.index(song_name)  # assinging the index number to variable i
+            # assinging the index number to variable i
+            i = titles.index(song_name)
+            # returning info
             return ("\nThe song: '" + db.track_name[i] + "' is made by '" +
                     db.track_artist[i] + "'.\nIt was released in the album '" +
                     db.track_album_name[i] + "', in the date " +
-                    db.track_album_release_date[i] + ", and it appeared in the spotify playlist named " +
+                    db.track_album_release_date[i] + ", and it appeared" +
+                    " in the spotify playlist named " +
                     db.playlist_name[i] + ", with other song of genre " +
                     db.playlist_genre[i] + ", and subgenre " +
                     db.playlist_subgenre[i] + ".\nThe song has " +
                     str(round(db.tempo[i])) + " bpm and it lasts " +
-                    str(round(db.duration_ms[i] / 1000)) + " seconds.\n")  # returning info
+                    str(round(db.duration_ms[i] / 1000)) + " seconds.\n")
         else:
-            return (" The song you're looking for is not present in our database. " +
+            return (" The song you're looking for" +
+                    " is not present in our database. " +
                     " Here you are some recommended songs we have:\n " +
                     " 'Party Rock Anthem',\n " +
                     " 'Symphony Of Destruction',\n " +
                     " 'Some Nights'! ")
 
-    def karaoke(self):  # defining the function to create a karaoke
+    # defining the function to create a karaoke
+    def karaoke(self):
         song_name = self.song_name.lower().strip()
         db = pd.read_csv(self.path)
-        titles = list(db["track_name"].str.lower())  # creating a list with all song titles
+        # creating a list with all song titles
+        titles = list(db["track_name"].str.lower())
 
         if song_name in titles:
-            i = titles.index(song_name)  # assinging the index number to variable i
+            # assinging the index number to variable i
+            i = titles.index(song_name)
+            # returning song URL and song lyrics
             return (" \nType this in your favourite browser: 'spotify:track:" +
                     str(db.track_id[i]) + "' and sing with our lyrics!\n\n " +
-                    db.track_name[i].upper() + " - " + db.track_artist[i].upper() + "\n\n" +
-                    str(db.lyrics[i]))  # returning song URL and song lyrics
+                    db.track_name[i].upper() +
+                    " - " + db.track_artist[i].upper() +
+                    "\n\n" +
+                    str(db.lyrics[i]))
         else:
-            return ("Not found!\nHere you are some recommended songs we have:\n" +
+            # returning recommendations, if song not present
+            return ("Not found!\nHere you are" +
+                    " some recommended songs we have:\n" +
                     "'Party Rock Anthem',\n" +
                     "'Symphony Of Destruction',\n" +
-                    "'Some Nights'!")  # returning recommendations, if song not present
+                    "'Some Nights'!")
--- modules/getsong.py
+++ modules/getsong.py
@@ -13,22 +13,25 @@ import pandas as pd
 
 # creating a class named Lyrics
 class Lyrics:
-
-    def __init__(self, lyr, path):  # initializing the object's attributes
+    # initializing the object's attributes
+    def __init__(self, lyr, path):
         self.lyr = lyr
         self.path = path
 
-    def getsong(self):  # function to return the song in which the text is found
+    # function to return the song in which the text is found
+    def getsong(self):
         lyr = str(self.lyr)
         lyr = self.lyr.lower()  # lowering user string input
 
         # handling wrong input
         wrong_input = ["", " "]
         if str(lyr) in wrong_input:
-            return "Empty input is not supported by the program. Try with something different!"
+            return ("Empty input is not supported by the program." +
+                    " Try with something different!")
 
         db = pd.read_csv(self.path)
-        all_lyr = list(db["lyrics"])  # creating a list with all lyrics of all songs
+        # creating a list with all lyrics of all songs
+        all_lyr = list(db["lyrics"])
         for i in range(len(all_lyr)):
             all_lyr[i] = all_lyr[i].lower()  # lowering all lyrics of the list
         n = 0
@@ -36,19 +39,29 @@ class Lyrics:
         if "mypackage" in self.path:
             print("This could take a while...\n")
         res = ""
-
-        for i in range(len(all_lyr)):  # iterating through each lyrics of the dataset
-            for j in range(len(all_lyr[i]) - len(lyr)+1):  # accessing each set of words of len(lyr)
-                if lyr == all_lyr[i][j:j + len(lyr)]:  # comparing user input & lyrics
-                    res += db["track_name"][i].upper() + " - " + db["track_artist"][i].upper() + "\n"
+        # iterating through each lyrics of the dataset
+        for i in range(len(all_lyr)):
+            # accessing each set of words of len(lyr)
+            for j in range(len(all_lyr[i]) - len(lyr)+1):
+                # comparing user input & lyrics
+                if lyr == all_lyr[i][j:j + len(lyr)]:
+                    res += db["track_name"][i].upper() + " - " + \
+                        db["track_artist"][i].upper() + "\n"
                     n += 1
                     if n >= 50:  # setting execution stopper at 50th result
-                        return (res + "\nExecution stopped at 50th matching result.\n" + "There are more than 50 songs with this input.\n")
+                        return (res +
+                                "\nExecution" +
+                                " stopped at 50th matching result.\n" +
+                                "There are more than 50" +
+                                " songs with this input.\n")
                     break
 
         if n == 0:  # Recommendations if lyrics not found
-            return ("So sorry! '" + lyr + "' seems not to be found in any song inside our database.\n\n" +
-                    "You could try with other lyrics, try these for example:\n" +
+            return ("So sorry! '" + lyr +
+                    "' seems not to be found" +
+                    " in any song inside our database.\n\n" +
+                    "You could try with other lyrics," +
+                    " try these for example:\n" +
                     "'Push it out, fake a smile'\n" +
                     "'I just wanna stay in the sun where I find'")
 
